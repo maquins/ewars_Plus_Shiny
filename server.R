7f9fc9f9789020e2092635808aad72c66369ab86
maquins <36748766+maquins@users.noreply.github.com>
2022-08-23 08:39:24 +0200

    Update server.R
    increase upload limit

--- server.R
+++ server.R
@@ -5,7 +5,8 @@
     #install.packages(pkgs[i])
   #}
 #}
-
+options(shiny.maxRequestSize=250*1024^2)
+getShinyOption("shiny.maxRequestSize")
 
 suppressMessages(library(plyr,warn.conflicts =F))## This package should be loaded before dplyr
 suppressMessages(library(dplyr,warn.conflicts =F))
